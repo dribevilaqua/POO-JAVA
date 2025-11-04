62ffc05198de9af8b475162d869fa82d8f68bd76
ANDRIELE BEVILAQUA GABRIEL <1090482411028@fatec-srv.local>
2025-11-04 11:04:04 -0300

    04/11/2025

--- ExemploBD/ExemploBD/src/main/java/br/edu/fatecfranca/ExemploBD/controller/ProdutoController.java
+++ ExemploBD/ExemploBD/src/main/java/br/edu/fatecfranca/ExemploBD/controller/ProdutoController.java
@@ -3,11 +3,10 @@ package br.edu.fatecfranca.ExemploBD.controller;
 import br.edu.fatecfranca.ExemploBD.model.Produto;
 import br.edu.fatecfranca.ExemploBD.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController //Responsável por requisições REST
 @RequestMapping("/produto") // endpoint da API
@@ -19,5 +18,25 @@ public class ProdutoController {
     @GetMapping
     public List<Produto> listaTodos(){
         return produtoService.listaTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Produto> buscaPorId(@PathVariable Long id){
+        return produtoService.listaPorId(id);
+    }
+
+    @PostMapping
+    public Produto insere(@RequestBody Produto produto){
+        return produtoService.insere(produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean remover(@PathVariable Long id){
+        return produtoService.remover(id);
+    }
+
+    @PutMapping("/{id}")
+    public Produto atualizar(@PathVariable Long id, @RequestBody Produto novoProduto){
+        return produtoService.atualizar(id, novoProduto);
     }
 }
--- ExemploBD/ExemploBD/src/main/java/br/edu/fatecfranca/ExemploBD/service/ProdutoService.java
+++ ExemploBD/ExemploBD/src/main/java/br/edu/fatecfranca/ExemploBD/service/ProdutoService.java
@@ -3,6 +3,8 @@ package br.edu.fatecfranca.ExemploBD.service;
 import br.edu.fatecfranca.ExemploBD.model.Produto;
 import br.edu.fatecfranca.ExemploBD.repository.ProdutoRepository;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,4 +21,37 @@ public class ProdutoService {
         // SELECT * FROM tb_produto
         return produtoRepository.findAll();
     }
+
+    //método que consulta um produto em particular
+    public Optional<Produto> listaPorId(Long id){
+        // se o findById() encontrar um produto, retorna ele,
+        // se o findById() não encontrar retorna null
+        // select * from tb_produto wher tb_produto.id = id
+        return produtoRepository.findById(id);
+
+    }
+
+    //metodo para inserir um produto na tabela tb_produto
+    public Produto insere (Produto produto){
+        //insert into tb_produto values (...)
+        return produtoRepository.save(produto);
+    }
+
+    //método pra remover um produto da tabela tb_produto
+    public boolean remover(Long id) {
+        if (produtoRepository.existsById(id)) { //verifica se o produto existe
+            produtoRepository.deleteById(id);
+            return true;
+        }
+        return false; //não encontrou
+    }
+    public Produto atualizar(Long id, Produto novoProduto){
+        //novoProduto não tem id
+        if (produtoRepository.existsById(id)) { //verifica se o produto existe
+            novoProduto.setId(id); //atualizo novoProduto com o id
+            return produtoRepository.save(novoProduto); //atualiza e retorna produto atual.
+        }
+        return null; //caso o produto não exista
+    }
+
 }
